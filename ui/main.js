0a182ce3f41c3801235ed9908c6a5bc97eb43637
ramyakavuri <kavuriramya.cse@gmail.com>
2016-09-27 16:34:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ var element = document.getElementById('main-text');
 element.innerHTML='new value';
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function(){
-  img.style.marginLeft = '100px'; 
+    var intervel = setIntervel(moveRight, 100);
+  //img.style.marginLeft = '100px'; 
 };
